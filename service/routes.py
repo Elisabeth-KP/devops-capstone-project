8cde1de18fc0180f905ffe627ff00370fb5dded3
Elisabeth-KP <elisabeth.pedro@ideiasdinamicas.com>
2026-05-05 17:12:14 +0100

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -89,7 +89,29 @@ def get_accounts(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_account(account_id):
+
+    app.logger.info("Request to update Account with id: %s", account_id)
+
+    account = Account.find(account_id)
+
+    # 404 if not found
+    if not account:
+        abort(404, f"Account with id [{account_id}] not found")
+
+    # Get JSON data from request
+    data = request.get_json()
+
+    # Update fields
+    account.name = data.get("name", account.name)
+    account.email = data.get("email", account.email)
+    account.address = data.get("address", account.address)
+
+    # Save changes
+    account.update()
+
+    return account.serialize(), 200
 
 
 ######################################################################
