86665ca611284dc94c6131b81cda7e5ca4354e7a
Elisabeth-KP <elisabeth.pedro@ideiasdinamicas.com>
2026-05-05 17:20:24 +0100

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -118,7 +118,21 @@ def update_account(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 
-# ... place you code here to DELETE an account ...
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_account(account_id):
+
+    app.logger.info("Request to delete Account with id: %s", account_id)
+
+    account = Account.find(account_id)
+
+    # If not found → 404
+    if not account:
+        abort(404, f"Account with id [{account_id}] not found")
+
+    # Delete account
+    account.delete()
+
+    return "", 204
 
 
 ######################################################################
