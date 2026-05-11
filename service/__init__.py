474234dcdfe96c094ac97616dd1f57dc7d14a0ed
Elisabeth-KP <elisabeth.pedro@ideiasdinamicas.com>
2026-05-11 07:25:55 -0400

    Fix trailing whitespace

--- service/__init__.py
+++ service/__init__.py
@@ -10,7 +10,7 @@ from flask_talisman import Talisman
 from flask_cors import CORS
 from service import config
 from service.common import log_handlers
-from service.models import db   
+from service.models import db
 
 app = Flask(__name__)
 
