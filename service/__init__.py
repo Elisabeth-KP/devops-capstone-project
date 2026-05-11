d03f51cf71afbe2a9d7f46d343528e671c921f6b
Elisabeth-KP <elisabeth.pedro@ideiasdinamicas.com>
2026-05-11 12:02:31 +0100

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -20,7 +20,6 @@ app.config.from_object("service.config")
 db.init_app(app)
 talisman = Talisman(app, force_https=False)
 CORS(app)
-app.config.from_object(config)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
